be3d5cfc3bb68443f9bc987ba74165b876e15259
galinauskastomas-wq <galinauskastomas@gmail.com>
2026-06-02 18:32:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,24 @@ jobs:
       with:
         python-version: '3.11'
 
-    - name: Install Dependencies
+    - name: Install System Dependencies
       run: |
         sudo apt-get update
-        sudo apt-get install -y libsqlite3-dev lld cython3 virtualenv unzip
+        # Pridedame papildomus įrankius: autoconf, automake, libtool, patch
+        sudo apt-get install -y libsqlite3-dev lld cython3 virtualenv unzip openjdk-17-jdk wget autoconf automake libtool patch
         python -m pip install --upgrade pip
         pip install "buildozer>=1.5.0"
 
     - name: Build with Buildozer
       run: |
+        # Suteikiame pilnas teises aplankui prieš pradedant darbą
+        chmod -R 777 .
         YES=1 buildozer -v android debug
+      env:
+        ACCEPT_SDK_LICENSE_NET: "y"
 
+    - name: Upload APK Artifact
+      uses: actions/upload-artifact@v4
+      with:
+        name: WorkHoursApp-APK
+        path: bin/*.apk
